ede75d870b61c3ee54ca05520b43d788679795a9
jefersonrs2112 <jeferson.rs@hotmail.com>
2026-07-07 17:32:15 -0300

    Add Booster class with Name and Price properties

---
+++ src/YFM.Core/Entities/Base/Booster.cs
@@ -0,0 +1,11 @@
+using YFM.Core.Entities.Base;
+
+namespace YFM.Core.Entities
+{
+    public class Booster : EntityBase
+    {
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+    }
+}
