85e1d3bb2705374d103d4d1a5dff8558c3031fa1
jefersonrs2112 <jeferson.rs@hotmail.com>
2026-07-07 17:32:53 -0300

    Add BoosterCard class with properties

---
+++ src/YFM.Core/Entities/BoosterCard.cs
@@ -0,0 +1,11 @@
+namespace YFM.Core.Entities
+{
+    public class BoosterCard
+    {
+        public int BoosterId { get; set; }
+
+        public int CardId { get; set; }
+
+        public double Probability { get; set; }
+    }
+}
