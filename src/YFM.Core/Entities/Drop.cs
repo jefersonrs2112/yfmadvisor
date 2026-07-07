18cb21af0bddf56b21f5b3f11a31352b9bb594c6
jefersonrs2112 <jeferson.rs@hotmail.com>
2026-07-07 17:33:46 -0300

    Add Drop class with properties for Duelist and Card

---
+++ src/YFM.Core/Entities/Drop.cs
@@ -0,0 +1,13 @@
+namespace YFM.Core.Entities
+{
+    public class Drop
+    {
+        public int DuelistId { get; set; }
+
+        public int CardId { get; set; }
+
+        public char Rank { get; set; }
+
+        public double Probability { get; set; }
+    }
+}
