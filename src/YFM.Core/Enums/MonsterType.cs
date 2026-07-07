7e8b90a29eb20a02c87e51ea339d4f32ba5bde02
jefersonrs2112 <jeferson.rs@hotmail.com>
2026-07-07 16:10:06 -0300

    Create MonsterType.cs

---
+++ src/YFM.Core/Enums/MonsterType.cs
@@ -0,0 +1,26 @@
+namespace YFM.Core.Enums
+{
+    public enum MonsterType
+    {
+        Dragon,
+        Spellcaster,
+        Zombie,
+        Warrior,
+        BeastWarrior,
+        Beast,
+        WingedBeast,
+        Fiend,
+        Fairy,
+        Insect,
+        Dinosaur,
+        Reptile,
+        Fish,
+        SeaSerpent,
+        Machine,
+        Thunder,
+        Aqua,
+        Pyro,
+        Rock,
+        Plant
+    }
+}
