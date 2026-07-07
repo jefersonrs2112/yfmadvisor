01c514c83b8e6f0f49574977223a26240a878b0c
jefersonrs2112 <jeferson.rs@hotmail.com>
2026-07-07 16:29:18 -0300

    Add KnowledgeSource class with properties

---
+++ src/YFM.Core/Entities/KnowledgeSource.cs
@@ -0,0 +1,13 @@
+namespace YFM.Core.Entities
+{
+    public class KnowledgeSource
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public decimal Confidence { get; set; }
+    }
+}
