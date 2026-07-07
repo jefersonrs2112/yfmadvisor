4656117fbe0db951386ec1677f7a130531e1d418
jefersonrs2112 <jeferson.rs@hotmail.com>
2026-07-07 17:31:36 -0300

    Add TerrainModifier class with properties

---
+++ src/YFM.Core/Entities/TerrainModifier.cs
@@ -0,0 +1,11 @@
+namespace YFM.Core.Entities
+{
+    public class TerrainModifier
+    {
+        public int TerrainId { get; set; }
+
+        public int MonsterTypeId { get; set; }
+
+        public int Bonus { get; set; }
+    }
+}
