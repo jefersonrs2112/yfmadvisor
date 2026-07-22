447ef66274bec0b417bf38ae55c68ba59ea55d5c
jefersonrs2112 <jeferson.rs@hotmail.com>
2026-07-22 11:41:14 -0300

    Add FusionOperand class with properties

---
+++ src/YFM.Core/Entities/FusionOperand.cs
@@ -0,0 +1,11 @@
+using YFM.Core.Enums;
+
+namespace YFM.Core.Entities
+{
+    public class FusionOperand
+    {
+        public FusionOperandType Type { get; set; }
+
+        public int ValueId { get; set; }
+    }
+}
