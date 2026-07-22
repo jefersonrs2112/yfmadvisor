3c2e1f894fd3b0a413e24c6e9d3a2a6344acd42a
jefersonrs2112 <jeferson.rs@hotmail.com>
2026-07-22 11:42:38 -0300

    Add CardRoleAssignment class for card-role mapping

---
+++ src/YFM.Core/Entities/CardRoleAssigment.cs
@@ -0,0 +1,11 @@
+namespace YFM.Core.Entities
+{
+    public class CardRoleAssignment
+    {
+        public int CardId { get; set; }
+
+        public int RoleId { get; set; }
+
+        public decimal Weight { get; set; }
+    }
+}
